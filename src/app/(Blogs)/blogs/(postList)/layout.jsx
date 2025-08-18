b30d2a27734b86b3dc515792ec1c2c5699a4e853
khorshid <www.khorshid11@gmail.com>
2025-08-18 08:46:10 +0330

    Fix merge conflicts in responsive mobile layout

--- src/app/(Blogs)/blogs/(postList)/layout.jsx
+++ src/app/(Blogs)/blogs/(postList)/layout.jsx
@@ -4,11 +4,7 @@ import Header from "./_components/Header";
 
 function layout({ children }) {
   return (
-<<<<<<< HEAD
-    <div className="container mx-auto py-5 px-4 space-y-6">
-=======
     <div className="container mx-auto overflow-x-hidden py-5 px-4 space-y-6">
->>>>>>> fix resposive mobile bugs
       <Header />
       <div className="grid grid-cols-12 gap-6">
         <aside className="col-span-12 lg:col-span-3 text-sky-900 space-y-4">
